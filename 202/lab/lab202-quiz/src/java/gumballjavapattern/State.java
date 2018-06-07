3ef4c6e2f37e065bf10bf92cfa243c551dad63ac
Lin Cheng <lcheng@fortinet.com>
2018-06-07 17:28:55 +0800

    202 labs initial commit.

---
+++ 202/lab/lab202-quiz/src/java/gumballjavapattern/State.java
@@ -0,0 +1,9 @@
+package gumballjavapattern;
+
+public interface State {
+ 
+	public void insertQuarter();
+	public void ejectQuarter();
+	public void turnCrank();
+	public void dispense();
+}
---
+++ 202/lab/lab202-quiz/src/java/gumballjavatypical/Main.java
@@ -0,0 +1,22 @@
+package gumballjavatypical;
+
+public class Main {
+    public static void main(String[] args) {
+        GumballMachine gumballMachine = new GumballMachine(5);
+
+        System.out.println(gumballMachine);
+
+        gumballMachine.insertQuarter( 25 );
+        gumballMachine.turnCrank();
+
+        System.out.println(gumballMachine);
+
+        gumballMachine.insertQuarter( 25 );
+        gumballMachine.turnCrank();
+        gumballMachine.insertQuarter( 25 );
+        gumballMachine.turnCrank();
+
+        System.out.println(gumballMachine);
+    }
+
+}
