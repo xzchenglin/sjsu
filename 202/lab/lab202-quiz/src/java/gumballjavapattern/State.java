2cb2831239285cb4ea6174c41a4e2642d467c3de
Lin Cheng <lcheng@fortinet.com>
2018-07-06 15:11:32 +0800

    rm

--- 202/lab/lab202-quiz/src/java/gumballjavapattern/State.java
+++
@@ -1,9 +0,0 @@
-package gumballjavapattern;
-
-public interface State {
- 
-	public void insert(GumballMachine.COIN coin);
-	public void ejectQuarter();
-	public void turnCrank();
-	public void dispense();
-}
